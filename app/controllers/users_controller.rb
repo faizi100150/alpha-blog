fa56d4e8b626903dbfa0949afba55b8c98137754
Alpha Blog <faizanamin982@gmail.com>
2020-04-15 12:15:26 +0500

    Complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,57 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit , :update , :show]
+  before_action :require_same_user, only: [:edit, :update]
   def new
     @user = User.new
   end
 
-def create
-    @user = User.new(user_params)
-  if @user.save
-    flash[:success] = "Welcome to the Alpha blog #{@user.username}"
-    redirect_to articles_path
+  def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "Welcome to the Alpha blog #{@user.username}"
+        redirect_to articles_path
+        else
+        render 'new'
+      end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update (user_params)
+      flash[:success] = "Your Account was updated successfully"
+      redirect_to articles_path
+
     else
-    render 'new'
+      render 'edit'
+
+    end
+
   end
-end
 
-def edit
-  @user = User.find(params[:id])
-end
+  def show
+  end
 
-def update
+  def set_user
     @user = User.find(params[:id])
-  if @user.update (user_params)
-    flash[:success] = "Your Account was updated successfully"
-    redirect_to articles_path
-
-  else
-    render 'edit'
-
   end
 
-end
-
-def show
-  @user=User.find(params[:id])
-end
-
-def index
- @users = User.all
-end
+  def index
+   @users = User.all
+  end
 
 private
   def user_params
     params.require(:user).permit(:username , :email , :password)
   end
 
+  def require_same_user
+    if  current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
+  end
 
 end
